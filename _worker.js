d32095d29be5505206640db2d1a6ccf091653713
tixiaohanmc <106894972+tixiaohanmc@users.noreply.github.com>
2022-09-02 22:53:04 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'app.proxy-cheap.com'
+      url.hostname = 'fastconnect.tk:2082'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
